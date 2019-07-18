e1e88fcbdf3d9b449c9d05f93a38c3a7dd959caa
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-07-18 19:17:58 -0400

    day2

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,33 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+
+
 def eating_cookies(n, cache=None):
-  pass
+
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    ways = 0
+    for i in range(1, 4):
+        if cache is not None:
+            if cache[n - i] == 0:
+                cache[n - i] = eating_cookies(n - i, cache)
+            ways += cache[n - i]
+        else:
+            ways += eating_cookies(n - i)
+    if cache is not None:
+        cache[n] = ways
+    return ways
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".
+              format(ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -5,22 +5,37 @@ from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
+
 def knapsack_solver(items, capacity):
-  pass
-  
+    cache = [[{} for k in range(capacity + 1)] for k in range(len(items) + 1)]
+    for i in range(len(items) + 1):
+        for j in range(capacity + 1):
+            if i == 0 or j == 0:
+                cache[i][j] = {'Value': 0, 'Chosen': []}
+            elif items[i - 1].size > j:
+                cache[i][j] = cache[i - 1][j]
+            else:
+                first_case = cache[i - 1][j]
+                second_case = {'Value': cache[i-1][j-items[i-1].size]['Value'] + items[i-1].value,
+                               'Chosen': cache[i-1][j-items[i-1].size]['Chosen'] + [items[i-1].index]}
+                if first_case['Value'] > second_case['Value']:
+                    cache[i][j] = first_case
+                else:
+                    cache[i][j] = second_case
+    return cache[len(items)][capacity]
 
 if __name__ == '__main__':
-  if len(sys.argv) > 1:
-    capacity = int(sys.argv[2])
-    file_location = sys.argv[1].strip()
-    file_contents = open(file_location, 'r')
-    items = []
+    if len(sys.argv) > 1:
+        capacity = int(sys.argv[2])
+        file_location = sys.argv[1].strip()
+        file_contents = open(file_location, 'r')
+        items = []
 
-    for line in file_contents.readlines():
-      data = line.rstrip().split()
-      items.append(Item(int(data[0]), int(data[1]), int(data[2])))
-    
-    file_contents.close()
-    print(knapsack_solver(items, capacity))
-  else:
-    print('Usage: knapsack.py [filename] [capacity]')
+        for line in file_contents.readlines():
+            data = line.rstrip().split()
+            items.append(Item(int(data[0]), int(data[1]), int(data[2])))
+
+        file_contents.close()
+        print(knapsack_solver(items, capacity))
+    else:
+        print('Usage: knapsack.py [filename] [capacity]')
